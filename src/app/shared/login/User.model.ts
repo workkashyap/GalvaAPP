f0a546afa52a95947db46f13cefa1279d55d8be6
BarkhaJV <barkhau23@gmail.com>
2020-02-06 17:57:12 +0530

    Header search

--- src/app/shared/login/User.model.ts
+++ src/app/shared/login/User.model.ts
@@ -1,9 +1,8 @@
 export class User {
- id: number;
- username: string;
- password: string;
- computername: string;
- ipaddress: string;
- token?: string;
-
+  id: number;
+  username: string;
+  password: string;
+  computername: string;
+  ipaddress: string;
+  token?: string;
 }
