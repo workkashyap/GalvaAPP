3d8351a1ae4052a03842e97669212ff46f93007d
workkashyap <workkashyap@gmail.com>
2020-02-10 18:52:29 +0530

    bug solved

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,35 +1,21 @@
-<<<<<<< HEAD
-import { Component, OnInit, ViewChild  } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import { FullCalendarComponent } from '@fullcalendar/angular';
-import { DatePipe } from '@angular/common';
+import { PlantService } from '../shared/plant/plant.service';
 
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html',
-  styleUrls: ['./calendar.component.css'],
-  providers: [DatePipe]
-=======
-import { Component, OnInit, ViewChild } from "@angular/core";
-import dayGridPlugin from "@fullcalendar/daygrid";
-import { FullCalendarComponent } from "@fullcalendar/angular";
-import { PlantService } from "../shared/plant/plant.service";
-
-@Component({
-  selector: "app-calendar",
-  templateUrl: "./calendar.component.html",
-  styleUrls: ["./calendar.component.css"]
->>>>>>> 137d00bd2a404879be33f5e60e11f8f50aa55d22
+  styleUrls: ['./calendar.component.css']
 })
 export class CalendarComponent implements OnInit {
   calendarPlugins = [dayGridPlugin];
-  @ViewChild("calendar", { static: false })
+  @ViewChild('calendar', { static: false })
   calendarComponent: FullCalendarComponent;
   calendarApi: any;
   options: any;
   cdate: Date;
   public startdate: string;
-  public datePipe: DatePipe;
   public rejectdata: any = [
     { title: 'event 1', date: '2020-02-08', backgroundColor: '#da532c', borderColor: '#da532c'},
     { title: 'event 2', date: '2020-02-09' },
