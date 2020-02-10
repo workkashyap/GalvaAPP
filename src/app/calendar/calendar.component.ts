090e6f210f400f8506ff40af665d3f4eb3bb1838
workkashyap <workkashyap@gmail.com>
2020-02-10 16:49:25 +0530

    Done

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild  } from '@angular/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
+import { FullCalendarComponent } from '@fullcalendar/angular';
 
 @Component({
   selector: 'app-calendar',
@@ -7,14 +8,45 @@ import dayGridPlugin from '@fullcalendar/daygrid';
   styleUrls: ['./calendar.component.css']
 })
 export class CalendarComponent implements OnInit {
-  calendarPlugins = [dayGridPlugin]; // important!
+  
+  calendarPlugins = [dayGridPlugin];
+  @ViewChild('calendar', {static: false}) calendarComponent: FullCalendarComponent;
+  calendarApi: any;
+  options: any;
   public rejectdata: any = [
     { title: 'event 1', date: '2020-02-08' },
     { title: 'event 2', date: '2020-02-09' },
   ];
-  constructor() { }
+  constructor() { 
+    this.options = {
+
+      editable: true,
+      // header: {
+      //   left: 'prev,next today',
+      //   center: 'title',
+      //   right: 'dayGridMonth, listDay'
+      // },
+      header: {
+        left:   'title',
+        center: '',
+        right:  'today',
+        height: 500
+    },
+      plugins: [dayGridPlugin],
+    };
+  }
 
   ngOnInit() {
   }
-
+  handleDateClick(arg) { // handler method
+    alert(arg.dateStr);
+  }
+  Next() {
+    this.calendarApi = this.calendarComponent.getApi();
+    this.calendarApi.next();
+  }
+  Previous() {
+    this.calendarApi = this.calendarComponent.getApi();
+    this.calendarApi.prev();
+  }
 }
