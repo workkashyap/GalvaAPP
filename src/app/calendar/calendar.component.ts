137d00bd2a404879be33f5e60e11f8f50aa55d22
BarkhaJV <barkhau23@gmail.com>
2020-02-10 18:44:12 +0530

    Calender height  and  margins

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,37 +1,40 @@
-import { Component, OnInit, ViewChild  } from '@angular/core';
-import dayGridPlugin from '@fullcalendar/daygrid';
-import { FullCalendarComponent } from '@fullcalendar/angular';
+import { Component, OnInit, ViewChild } from "@angular/core";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import { FullCalendarComponent } from "@fullcalendar/angular";
+import { PlantService } from "../shared/plant/plant.service";
 
 @Component({
-  selector: 'app-calendar',
-  templateUrl: './calendar.component.html',
-  styleUrls: ['./calendar.component.css']
+  selector: "app-calendar",
+  templateUrl: "./calendar.component.html",
+  styleUrls: ["./calendar.component.css"]
 })
 export class CalendarComponent implements OnInit {
-  
   calendarPlugins = [dayGridPlugin];
-  @ViewChild('calendar', {static: false}) calendarComponent: FullCalendarComponent;
+  @ViewChild("calendar", { static: false })
+  calendarComponent: FullCalendarComponent;
   calendarApi: any;
   options: any;
   public rejectdata: any = [
-    { title: 'event 1', date: '2020-02-08' },
-    { title: 'event 2', date: '2020-02-09' },
+    { title: "event 1", date: "2020-02-08" },
+    { title: "event 2", date: "2020-02-09" }
   ];
-  constructor() { 
+  constructor(public plantservice: PlantService) {
     this.options = {
       editable: true,
       header: {
-        left:   'title',
-        center: '',
-        right:  'today'
-    },
-      plugins: [dayGridPlugin],
+        left: "title",
+        center: "",
+        right: "today"
+      },
+      plugins: [dayGridPlugin]
     };
   }
 
   ngOnInit() {
+    this.plantservice.getPlantData();
   }
-  handleDateClick(arg) { // handler method
+  handleDateClick(arg) {
+    // handler method
     alert(arg.dateStr);
   }
   Next() {
