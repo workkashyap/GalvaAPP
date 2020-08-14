1c27e133ff4d58c744fd62e979d721f51a88ff82
unknown <rahul@gnhub.com>
2020-08-14 19:41:38 +0530

    bug solve

--- src/app/shared/mouldconscalendar/mouldconscalendar.service.spec.ts
+++ src/app/shared/mouldconscalendar/mouldconscalendar.service.spec.ts
@@ -1,12 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 
-import { MouldproductionService } from './mouldproduction.service';
+import { MouldconscalendarService } from './mouldconscalendar.service';
 
 describe('PurchaseService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
-    const service: MouldproductionService = TestBed.get(MouldproductionService);
+    const service: MouldconscalendarService = TestBed.get(MouldconscalendarService);
     expect(service).toBeTruthy();
   });
 });
