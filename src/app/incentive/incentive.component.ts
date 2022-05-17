df6c3ac7a022ae825ecedeb0ba7a39f4040cc2fe
Meet Patel <smk123321@gmail.com>
2022-05-17 22:02:12 +0530

    incentive update

--- src/app/incentive/incentive.component.ts
+++ src/app/incentive/incentive.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Grid, GridOptions } from 'ag-grid-community';
 import { Observable } from 'rxjs';
 import { ColDef, GridReadyEvent } from 'ag-grid-community';
 import { IncentiveService } from '../shared/incentive/incentive.service';
@@ -71,6 +72,10 @@ public autoGroupColumnDef: ColDef = {
       checkbox: false,
   }
 
+};
+
+public gridOptions: GridOptions = {
+  // pivotRowTotals: 'before',
 };
 
 
@@ -155,7 +160,7 @@ daysInMOnth(x, y) {
 }
 
 MyYearPivotComparator(a: string, b: string) {
-  const requiredOrder = ['1', '2', '3', '4', '5', '6', '7','8','9','10', '11', '12', '13' ,'14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31'];
+  const requiredOrder = ['31', '30', '29', '28', '27', '26', '25', '24', '23', '22', '21', '20', '19' ,'18','17','16','15','14','13','12','11','10','9','8','7','6','5','4','3','2','1'];
   return requiredOrder.indexOf(a) - requiredOrder.indexOf(b);
 }
 
